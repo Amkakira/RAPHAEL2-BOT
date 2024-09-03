b83b8351528d60e998bab010e9ce5b41c7901657
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-09-03 16:56:40 +0300

    Update منشن.js

--- plugins/منشن.js
+++ plugins/منشن.js
@@ -1,4 +1,6 @@
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
