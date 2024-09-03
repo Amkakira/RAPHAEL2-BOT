cee4deec451b2f29f3f5b862b2215dc89bd6e66c
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-09-03 16:28:22 +0300

    Update بنك.js

--- plugins/بنك.js
+++ plugins/بنك.js
@@ -1,5 +1,5 @@
 let handler = async (m, {conn, usedPrefix}) => {
-	
+    let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     if (!(who in global.db.data.users)) throw `*❖┃الــمــســـتـــخــدم لــيــس ف قــاعــدة الــبــيــانــات┃⁉️ ❯*`
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix}) => {
 
 *⌘┇الــجــرعــات💊⇠ _${user.potion}_ ┇*
 *❐═━━━═╊⊰🐉⊱╉═━━━═❐*
-`, fkontak , m, { mentions: [who] })
+`, fkontak , m , { mentions: [who] })
 }
 handler.help = ['balance']
 handler.tags = ['econ']
